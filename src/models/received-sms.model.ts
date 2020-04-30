0d0edaf050f9fa9f419ffda91ef03ad3b03937ae
Rendy Harimayco <harimayco@users.noreply.github.com>
2020-05-01 02:09:31 +0700

    Update received-sms.model.ts

--- src/models/received-sms.model.ts
+++ src/models/received-sms.model.ts
@@ -2,5 +2,5 @@ import { SMS } from './sms.model';
 
 export interface ReceivedSMS extends SMS {
   id: number /** id position in phone memory or SIM memory */;
-  submitTime: Date | null /** <scts> - arrival time of the message to the SC */;
+  submitTime: string | null /** <scts> - arrival time of the message to the SC */;
 }
